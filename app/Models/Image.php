fd7029ebfa81dcba2a15f2827a8fe32fda7d0141
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-11-09 23:33:11 +0000

    added more details for each post

---
+++ app/Models/Image.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    const TYPE_PROFILE = 'Profile';
+    const TYPE_POST_TITLE = 'PostTitle';
+    const TYPE_POST_CONTENT = 'PostContent';
+
+    const DEFAULT_IMAGES = [
+        self::TYPE_PROFILE => 'profile/default-profile-image.jpg',
+        self::TYPE_POST_TITLE => 'post/default-post-title-image.jpg',
+        self::TYPE_POST_CONTENT => 'post/default-post-content-image.jpg',
+    ];
+
+    protected $table = 'image';
+
+    protected $fillable = [
+        'path',
+        'image_type',
+        'news_post_id',
+        'user_id',
+    ];
+
+    public function getPath()
+    {
+        if ($this->path && Storage::disk('public_uploads')->exists($this->path)) {
+            return $this->path;
+        } else {
+            return self::DEFAULT_IMAGES[$this->image_type];
+        }
+    }
+}
--- app/Models/NewsPost.php
+++ app/Models/NewsPost.php
@@ -4,22 +4,48 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 use Carbon\Carbon;
 
 class NewsPost extends Model
 {
     use HasFactory;
-    
+
     protected $table = 'news_post';
 
     protected $fillable = [
-        'title', 'content', 'author_id', 'for_followers', 'upvotes', 'downvotes', 'is_omitted'
+        'title',
+        'content',
+        'author_id',
+        'for_followers',
+        'upvotes',
+        'downvotes',
+        'is_omitted'
     ];
 
     public function author()
     {
         return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'news_post_tag', 'news_post_id', 'tag_id');
+    }
+
+    public function getTagNamesAttribute()
+    {
+        return $this->tags()->pluck('name')->toArray();
+    }
+
+    public function titleImage()
+    {
+        return $this->hasOne(Image::class, 'news_post_id')
+                    ->where('image_type', Image::TYPE_POST_TITLE);
+    }
+
+    public function getTitleImagePathAttribute()
+    {
+        return $this->titleImage->getPath();
     }
 
     // time_ago shows time in readable format
