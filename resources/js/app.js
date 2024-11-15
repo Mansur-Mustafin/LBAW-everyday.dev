94fbf4182d55b3af07f77c690794192cd0f0c8a2
DGoiana <diogogoianam@gmail.com>
2024-11-15 19:39:47 +0000

    adding tags

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 const thumbnailButton = document.getElementById('personalizedFileInput')
 const fileInput = document.getElementById('realFileInput')
+const deleteThumbnailButton = document.getElementById('deleteThumbnail')
+const tagSelector = document.getElementById('tagSelector')
+const selectedTags = document.getElementById('selectedTags')
 
 thumbnailButton.addEventListener('click', function (evt) {
    evt.preventDefault()
@@ -8,11 +11,35 @@ thumbnailButton.addEventListener('click', function (evt) {
 
 fileInput.addEventListener('change', function (evt) {
    const [file] = fileInput.files
-   console.log("ueue")
    if (file) {
-      console.log("ueue")
-      thumbnailButton.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
-      thumbnailButton.style.backgroundSize = 'cover';
+      thumbnailButton.style.backgroundImage = `url(${URL.createObjectURL(file)})`
+      thumbnailButton.style.backgroundSize = 'cover'
       thumbnailButton.innerHTML = ''
+      deleteThumbnailButton.style.display = 'block'
+   }
+})
+
+deleteThumbnailButton.addEventListener('click', function (evt) {
+   evt.preventDefault()
+   thumbnailButton.style.backgroundImage = ''
+   thumbnailButton.innerHTML = 'Thumbnail'
+   deleteThumbnailButton.style.display = 'none'
+})
+
+tagSelector.addEventListener('change', function () {
+   const selectedTag = tagSelector.value
+   if (selectedTag) {
+      const tagElement = document.createElement('div')
+      tagElement.textContent = '#' + selectedTag.toLowerCase()
+      tagElement.className = 'bg-input rounded-2xl p-3'
+      selectedTags.appendChild(tagElement)
+
+      for (let i = 0; i < tagSelector.options.length; i++) {
+         if (tagSelector.options[i].value === selectedTag) {
+            tagSelector.remove(i);
+            tagSelector.value = ''
+            break;
+         }
+      }
    }
 })
