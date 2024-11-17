e0f46035a8487ee23f282848d4802ec2ff38ef63
Rubem Neto <rubemviscard2635@gmail.com>
2024-11-17 23:31:59 +0000

    feat:search post page

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,14 +9,19 @@ use App\Models\User;
 
 class SearchController extends Controller
 {
-    public function search(Request $request) {
-        $search_query = $request->search;
-        $newsPosts = NewsPost::join('user','user.id','=',"news_post.id")
+    public function get_posts($search_query) 
+    {
+        return NewsPost::join('user','user.id','=',"news_post.id")
                 ->whereRaw('(tsvectors @@ plainto_tsquery(\'english\',?) OR title=?)',[$search_query,$search_query])
                 ->orderByRaw('ts_rank(tsvectors,plainto_tsquery(\'english\',?)) DESC',[$search_query])
                 ->get()
                 ->makeHidden('password');
+    }
 
+    public function search(Request $request) 
+    {
+        $search_query = $request->search;
+        $news_posts = SearchController::get_posts($search_query);
         $tag_query= $search_query == '' ? '' :"%".ucfirst($search_query)."%"; 
         $user_query= $search_query == '' ? '' : $search_query."%"; 
         $tags = Tag::where('name','like',$tag_query)->get();
@@ -26,9 +31,17 @@ class SearchController extends Controller
         $users = $users_from_username->merge($users_from_public_name);
 
         return response()->json([
-            "news_posts"=>$newsPosts,
+            "news_posts"=>$news_posts,
             "tags"=>$tags,
             "users"=>$users,
         ],200);
+    }
+
+    public function search_post(Request $request)
+    {
+        $search_query = $request->search;
+        $news_posts = SearchController::get_posts($search_query);
+
+        return view('pages.news',['news_posts'=>$news_posts]);
     }
 }
