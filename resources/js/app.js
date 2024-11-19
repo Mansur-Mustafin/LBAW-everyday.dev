6d8a838728605e9eb77d269c653705b85356927a
DGoiana <diogogoianam@gmail.com>
2024-11-19 09:50:08 +0000

    adding followers only toggle

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,85 +5,89 @@ const tagSelector = document.getElementById('tagSelector')
 const selectedTags = document.getElementById('selectedTags')
 const createForm = document.getElementById('createForm')
 const title = document.getElementById('title')
-
+const followersToggle = document.getElementById('toggleTwo')
+const hiddenToggle = document.getElementById('hiddenToggle')
 
 thumbnailButton.addEventListener('click', function (evt) {
-   evt.preventDefault()
-   fileInput.click()
+  evt.preventDefault()
+  fileInput.click()
+})
+
+followersToggle.addEventListener('change', function (evt) {
+  hiddenToggle.value = this.checked ? 'true' : 'false'
 })
 
 title.addEventListener('change', function (evt) {
-   title.style.borderWidth = '0px'
+  title.style.borderWidth = '0px'
 })
 
 fileInput.addEventListener('change', function (evt) {
-   const [file] = fileInput.files
-   if (file) {
-      thumbnailButton.style.backgroundImage = `url(${URL.createObjectURL(file)})`
-      thumbnailButton.style.backgroundSize = 'cover'
-      thumbnailButton.style.borderWidth = '0px'
-      thumbnailButton.innerHTML = ''
-      deleteThumbnailButton.style.display = 'block'
-   }
+  const [file] = fileInput.files
+  if (file) {
+    thumbnailButton.style.backgroundImage = `url(${URL.createObjectURL(file)})`
+    thumbnailButton.style.backgroundSize = 'cover'
+    thumbnailButton.style.borderWidth = '0px'
+    thumbnailButton.innerHTML = ''
+    deleteThumbnailButton.style.display = 'block'
+  }
 })
 
 deleteThumbnailButton.addEventListener('click', function (evt) {
-   evt.preventDefault()
-   thumbnailButton.style.backgroundImage = ''
-   thumbnailButton.innerHTML = 'Thumbnail'
-   deleteThumbnailButton.style.display = 'none'
-   fileInput.value = ''
+  evt.preventDefault()
+  thumbnailButton.style.backgroundImage = ''
+  thumbnailButton.innerHTML = 'Thumbnail'
+  deleteThumbnailButton.style.display = 'none'
+  fileInput.value = ''
 })
 
 tagSelector.addEventListener('change', function () {
-   const selectedTag = tagSelector.value
+  const selectedTag = tagSelector.value
 
-   if (selectedTag) {
-      const tagElement = document.createElement('div')
-      tagElement.dataset.tag = selectedTag;
-      tagElement.textContent = '#' + selectedTag.toLowerCase()
-      tagElement.className = 'bg-white rounded-xl p-2 text-input font-semibold text-sm'
-      selectedTags.appendChild(tagElement)
+  if (selectedTag) {
+    const tagElement = document.createElement('div')
+    tagElement.dataset.tag = selectedTag;
+    tagElement.textContent = '#' + selectedTag.toLowerCase()
+    tagElement.className = 'bg-white rounded-xl p-2 text-input font-semibold text-sm'
+    selectedTags.appendChild(tagElement)
 
-      for (let i = 0; i < tagSelector.options.length; i++) {
-         if (tagSelector.options[i].value === selectedTag) {
-            tagSelector.remove(i);
-            tagSelector.value = ''
-            break;
-         }
+    for (let i = 0; i < tagSelector.options.length; i++) {
+      if (tagSelector.options[i].value === selectedTag) {
+        tagSelector.remove(i);
+        tagSelector.value = ''
+        break;
       }
-   }
+    }
+  }
 })
 
 createForm.addEventListener('submit', function (evt) {
-   evt.preventDefault()
-   let post_tags = [];
+  evt.preventDefault()
+  let post_tags = [];
 
-   Array.from(selectedTags.children).forEach((child) => {
-      post_tags.push(child.dataset.tag);
-   });
+  Array.from(selectedTags.children).forEach((child) => {
+    post_tags.push(child.dataset.tag);
+  });
 
-   console.log(post_tags)
+  if (post_tags.length >= 0) document.getElementById('tagsInput').value = post_tags.join(',');
 
-   document.getElementById('tagsInput').value = post_tags.join(',');
+  if (!fileInput.files.length || !title.value.length) {
 
-   if (!fileInput.files.length || !title.value.length) {
+    if (!fileInput.files.length) {
+      thumbnailButton.style.borderWidth = '1px'
+      thumbnailButton.style.borderColor = 'red'
+    }
 
-      console.log(!title.value.length)
+    if (!title.value.length) {
+      console.log('teste')
+      title.style.borderWidth = '1px'
+      title.style.borderColor = 'red'
+    }
 
-      if (!fileInput.files.length) {
-         thumbnailButton.style.borderWidth = '1px'
-         thumbnailButton.style.borderColor = 'red'
-      }
+    return
+  }
 
-      if (!title.value.length) {
-         console.log('teste   ')
-         title.style.borderWidth = '1px'
-         title.style.borderColor = 'red'
-      }
-
-      return
-   }
-
-   createForm.submit()
+  console.log("File Input Value:", fileInput.files);
+  console.log("Title:", title.value);
+  console.log("value:", hiddenToggle.value);
+  createForm.submit()
 })
--- resources/views/pages/create-news.blade.php
+++ resources/views/pages/create-news.blade.php
@@ -2,38 +2,65 @@
 
 @section('content')
 <section class="flex flex-col border-x border-gray-700 self-center ml-64 mr-96 h-full">
-   <form class="px-3 flex flex-col gap-4 mt-4" method="POST" action="{{ route('news') }}" enctype="multipart/form-data"
-      id="createForm">
-      @csrf
-      <div class="flex">
-         <button class="bg-input rounded-3xl px-6 py-8 w-40 min-h-28" id="personalizedFileInput">Thumbnail</button>
-         <button id="deleteThumbnail" class="self-start hidden">
+<form class="px-3 flex flex-col gap-4 mt-4" method="POST" action="{{ route('news') }}" enctype="multipart/form-data"
+    id="createForm">
+    @csrf
+    <div class="flex">
+        <button class="bg-input rounded-3xl px-6 py-8 w-40 min-h-28" id="personalizedFileInput">Thumbnail</button>
+        <button id="deleteThumbnail" class="self-start hidden">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-               class="lucide lucide-circle-x">
-               <circle cx="12" cy="12" r="10" />
-               <path d="m15 9-6 6" />
-               <path d="m9 9 6 6" />
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            class="lucide lucide-circle-x">
+            <circle cx="12" cy="12" r="10" />
+            <path d="m15 9-6 6" />
+            <path d="m9 9 6 6" />
             </svg>
-         </button>
-      </div>
-      <input class="hidden" type="file" id="realFileInput" name="title_photo">
+        </button>
+    </div>
 
-      <input name="title" id="title" class="rounded-2xl bg-input outline-none p-3" placeholder="Post title*">
-      <textarea name="content" class="rounded-2xl bg-input outline-none p-3 min-h-40"
-         placeholder="Share your thoughts"></textarea>
-      <div class="flex items-center gap-3">
-         <select class="bg-input rounded-2xl p-3 w-40" id="tagSelector">
-            <option selected></option>
-            @foreach ($tags as $tag)
-            <option value={{ $tag['name'] }}>{{ $tag['name'] }}</option>
-         @endforeach
-         </select>
-         <label>Add tags to your post</label>
-      </div>
-      <div id="selectedTags" class="flex gap-1 flex-wrap"></div>
-      <input type="hidden" name="tags" id="tagsInput">
-      <button class="text-input bg-white font-bold rounded-xl px-6 py-2 self-end" type="submit">Post</button>
-   </form>
+
+    <input name="title" id="title" class="rounded-2xl bg-input outline-none p-3" placeholder="Post title*">
+    <textarea name="content" class="rounded-2xl bg-input outline-none p-3 min-h-40"
+        placeholder="Share your thoughts"></textarea>
+    <div class="flex items-center justify-between">
+        <div class='flex items-center gap-2'>
+            <select class="bg-input rounded-2xl p-3 w-40" id="tagSelector">
+                <option selected></option>
+                @foreach ($tags as $tag)
+                <option value={{ $tag['name'] }}>{{ $tag['name'] }}</option>
+            @endforeach
+            </select>
+            <label class='text-sm'>Add tags to your post</label>
+        </div>
+        <div>
+            <!-- -- code copied from https://tailgrids.com/components/toggle-switch -->
+            <label
+            for="toggleTwo"
+            class="flex items-center cursor-pointer select-none text-dark dark:text-white gap-2 text-sm"
+            >
+            <div class="relative">
+                <input
+                    type="checkbox"
+                    id="toggleTwo"
+                    class="peer sr-only"
+                        />
+                <div
+                    class="block h-8 rounded-full dark:bg-dark-2 bg-input w-14"
+                    ></div>
+                <div
+                    class="absolute w-6 h-6 transition bg-white rounded-full dot dark:bg-dark-4 left-1 top-1 peer-checked:translate-x-full peer-checked:bg-purple-900"
+                    ></div>
+            </div>
+            Followers only
+            </label>
+        </div>
+    </div>
+    <div id="selectedTags" class="flex gap-1 flex-wrap"></div>
+
+    <input class="hidden" name="tags" id="tagsInput">
+    <input class="hidden" type="file" id="realFileInput" name="title_photo">
+    <input class="hidden" type="text" id="hiddenToggle" name="for_followers" value='false'>
+    <button class="text-input bg-white font-bold rounded-xl px-6 py-2 self-end" type="submit">Post</button>
+</form>
 </section>
 @endsection
