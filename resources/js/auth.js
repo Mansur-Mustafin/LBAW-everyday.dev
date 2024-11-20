f658371cc2ab7e054b7dccde816bccc525200277
Mourinha08 <afonsomoura485@gmail.com>
2024-11-20 09:32:56 +0000

    changing eye toggle feature

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -1,23 +1,23 @@
-const hidePassword = (togglePasswordDiv,passwordFieldDiv) => {
-    document.getElementById(togglePasswordDiv).addEventListener('click', function () {
-        const passwordField = document.getElementById(passwordFieldDiv);
-        const eyeIcon = document.getElementById('eye-icon');
-
-        if (passwordField.type === 'password') {
-            passwordField.type = 'text';
-        } else {
-            passwordField.type = 'password';
-        }
-    });
+function togglePasswordVisibility(passwordField, toggleIcon) {
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        toggleIcon.textContent = 'visibility';
+    } else {
+        passwordField.type = 'password';
+        toggleIcon.textContent = 'visibility_off';
+    }
 }
 
-if(document.getElementById("toggle-password")) {
-    console.log('a')
-    hidePassword("toggle-password","password")
-}
+const togglePassword = document.getElementById('toggle-password');
+const passwordField = document.getElementById('password');
+const togglePasswordConfirm = document.getElementById('toggle-password-confirm');
+const passwordFieldConfirm = document.getElementById('password-confirm');
 
-if(document.getElementById("toggle-password-confirm")) {
-    console.log('b')
-    hidePassword("toggle-password-confirm","password-confirm")
-}
+togglePassword.addEventListener('click', () => {
+    togglePasswordVisibility(passwordField, togglePassword);
+});
+
+togglePasswordConfirm.addEventListener('click', () => {
+    togglePasswordVisibility(passwordFieldConfirm, togglePasswordConfirm);
+});
 
