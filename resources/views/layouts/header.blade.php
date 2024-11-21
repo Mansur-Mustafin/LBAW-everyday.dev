72bf538de7fea2eb854c2738d640a265703888f8
Rubem Neto <rubemviscard2635@gmail.com>
2024-11-21 16:24:57 +0000

    feat:user card on admin dashboard

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,12 +9,20 @@
         </div>
     </div>
 
+    <div class="flex gap-2">
     @if (Auth::check())
-        <a type="button" class="bg-white text-black rounded-xl px-6 py-2 font-bold mr-2" href="{{ url('/news/create-post') }}">
+        @if(Auth::user()->isAdmin())
+            <a type="button" class="bg-white text-black rounded-xl px-6 py-2 font-bold" href="{{ url('/admin') }}">
+                Dashboard
+            </a>
+        @endif
+        <a type="button" class="bg-white text-black rounded-xl px-6 py-2 font-bold" href="{{ url('/news/create-post') }}">
             New post
         </a>
         <a type="button" class="bg-white text-black rounded-xl px-2 py-2 font-bold" href="{{ url('/users/' . Auth::id()) . '/posts' }}">
             <img class="w-6 h-6" src="{{Auth::user()->profile_image_path}}" alt="">
         </a>
     @endif
+
+    </div>
 </header>
--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -1,27 +1,52 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container mx-auto">
-      <div class="flex flex-col border rounded divide-y divide-dashed">
-          <div class="grid grid-cols-7 p-3">
-            <div>Username</div>
-            <div>Public Name</div>
-            <div>Email</div>
-            <div>Rank</div>
-            <div>Status</div>
-            <div>Reputation</div>
-            <div>Admin</div>
-          </div>
-          @foreach ($users as $user)
-            <div class="grid grid-cols-7 p-2">
-              <div>{{ $user->username }}</div>
-              <div>{{ $user->public_name }}</div>
-              <div>{{ $user->email }}</div>
-              <div>{{ $user->rank }}</div>
-              <div>{{ $user->status }}</div>
-              <div>{{ $user->reputation }}</div>
-              <div>{{ $user->isAdmin() ? 'true' : 'false' }}</div>
-            </div>
-          @endforeach
+    <div class="container mx-auto flex flex-col gap-2 mt-4">
+
+      <h1 class="text-7xl border-b border-gray-700">
+        Users
+      </h1>
+
+      <div class="flex text-black bg-white rounded p-2 w-full mt-4">
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30"  viewBox="0 0 30 30">
+            <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
+        </svg>                
+        <input type="text" class="h-full p-2 outline-none rounded-t w-full text-xl">
       </div>
+
+      <a href="#" class="flex flex-col border text-white bg-black border-gray-700 rounded text-2xl items-center justify-center p-2">
+        <p class="w-fit">
+          Create new user
+        </p>
+      </a>
+      @foreach ($users as $user)
+      <div class="flex flex-col border border-gray-700 rounded">
+          <div class="flex justify-between p-2">
+            <div>
+              <h2 class="text-2xl flex gap-1">
+                <a href="{{ url("/users/{$user->id}/posts") }}">
+                  {{$user->public_name}}
+                </a>
+                {{-- Banned --}}
+                <span class="hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ban"><circle cx="12" cy="12" r="10"/><path d="m4.9 4.9 14.2 14.2"/></svg>
+                </span>
+                <span class="hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-dashed"><path d="M10.1 2.182a10 10 0 0 1 3.8 0"/><path d="M13.9 21.818a10 10 0 0 1-3.8 0"/><path d="M17.609 3.721a10 10 0 0 1 2.69 2.7"/><path d="M2.182 13.9a10 10 0 0 1 0-3.8"/><path d="M20.279 17.609a10 10 0 0 1-2.7 2.69"/><path d="M21.818 10.1a10 10 0 0 1 0 3.8"/><path d="M3.721 6.391a10 10 0 0 1 2.7-2.69"/><path d="M6.391 20.279a10 10 0 0 1-2.69-2.7"/></svg>
+                </span>
+                <span class="hidden">
+                {{-- Admin --}}
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shield"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/></svg>
+                </span>
+              </h1>
+              <h3 class="text-gray-400">{{ $user->username }}</h3>
+              <h3 class="text-gray-400">{{ $user->email }}</h3>
+            </div>
+            <a class="flex flex-col p-2 justify-center" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
+            </a>
+          </div>
+      </div>
+      @endforeach
     </div>
 @endsection
