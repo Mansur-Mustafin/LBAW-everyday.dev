178f01abcd522ef3f6d365c412e29d2a506dbe53
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-11-22 01:10:14 +0000

    feat: add loading indicator for infinite page

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -3,10 +3,14 @@
 @section('content')
     <div class="container tablet:mx-auto w-full">
         <h1 class="text-2xl font-semibold my-4">{{ $title }}</h1>
+        
         <div data-last_page="{{ $news_posts->lastPage() }}" data-url="{{ $baseUrl }}" id="news-posts-container" class="grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4 gap-4">
             @foreach ($news_posts as $news)
                 @include('partials.tile-post', ['news' => $news])
             @endforeach
         </div>
+        <div id="loading-icon" style="display: none;" class="my-6">
+            <img class="w-12 h-12 mx-auto" src="{{url('/assets/loading-icon.gif')}}" alt="Loading...">
+        </div>  
     </div>
 @endsection
