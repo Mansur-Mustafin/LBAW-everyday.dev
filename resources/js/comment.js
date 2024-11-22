87188db7b18b853d25b0fe6cb319c454ef8fe7c2
DGoiana <diogogoianam@gmail.com>
2024-11-22 23:41:55 +0000

    starting to work on nested comments

--- resources/js/comment.js
+++ resources/js/comment.js
@@ -1,5 +1,7 @@
 import { sendAjaxRequest } from './app.js'
 
+// TODO: profile image, add comment interaction when a comment is just added
+
 function createCommentElement(user, comment) {
    const newComment = document.createElement('div')
    newComment.classList.add('border', 'border-solid', 'border-gray-700', 'rounded-xl', 'flex', 'flex-col')
@@ -14,7 +16,7 @@ function createCommentElement(user, comment) {
        </div>
        <div class="mt-4">${comment}</div>
        <div class="mt-3 text-sm flex gap-3">
-         <a class="p-2 rounded-xl cursor-pointer hover:text-cyan-400 hover:bg-cyan-700 hover:bg-opacity-50">
+         <a class="p-2 rounded-xl cursor-pointer hover:text-cyan-400 hover:bg-cyan-700 hover:bg-opacity-50 sub-comment">
            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
              class="lucide lucide-message-circle">
@@ -44,6 +46,47 @@ function createCommentElement(user, comment) {
    return newComment
 }
 
+function createNestedCommentElement(user, comment, parent_id, id) {
+
+   const newComment = document.createElement('div');
+   newComment.classList.add('p-4');
+   newComment.className += ` ${comment.level == 1 && comment.replies.length == 0 ? '' : 'border-b border-solid border-gray-700 rounded-bl-xl'} ${comment.parent.replies.length <= 1 ? 'border-l border-opacity-50 border-solid border-gray-700' : 'rounded-xl'}`;
+
+   newComment.innerHTML = `
+       <div class="text-sm text-gray-400 flex gap-2">
+           <img src="${'#TODO'}">
+           <div class="flex flex-col">
+               <h2 class="text-white text-sm font-semibold">${user.public_name}</h2>
+               <h3 class="text-xs text-gray-500">@${user.username} · ${'Now'}</h3>
+           </div>
+       </div>
+       <div class="mt-4">
+           ${comment}
+       </div>
+       <div class="mt-3 text-sm flex gap-3">
+           <a class="p-2 rounded-xl cursor-pointer hover:text-cyan-400 hover:bg-cyan-700 hover:bg-opacity-50 sub-comment">
+               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-circle">
+                   <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
+               </svg>
+           </a>
+           <a class="p-2 rounded-xl cursor-pointer hover:text-rose-400 hover:bg-rose-700 hover:bg-opacity-50">
+               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark-plus">
+                   <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z" />
+                   <line x1="12" x2="12" y1="7" y2="13" />
+                   <line x1="15" x2="9" y1="10" y2="10" />
+               </svg>
+           </a>
+           <a class="p-2 rounded-xl cursor-pointer hover:text-purple-400 hover:bg-purple-700 hover:bg-opacity-50">
+               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link">
+                   <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+                   <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+               </svg>
+           </a>
+       </div>
+   `;
+   return newComment;
+}
+
 document.getElementById('commentForm').addEventListener('submit', function (e) {
    e.preventDefault()
 
@@ -64,8 +107,10 @@ function addCommentHandler() {
       const response = JSON.parse(this.responseText)
       const user = response.user
       const comment = response.comment
+      const parent_id = response.parent_id
+      const id = response.id
 
-      const newComment = createCommentElement(user, comment)
+      const newComment = response.type == 'non-nested' ? createCommentElement(user, comment) : createNestedCommentElement(user, comment, parent_id, id)
       document.getElementById('comment-section').prepend(newComment)
 
       commentInput.value = ''
@@ -79,9 +124,9 @@ document.querySelectorAll('.sub-comment').forEach(function (element) {
       form.classList.add('flex', 'items-center', '-mt-2')
 
       form.innerHTML = `
-         <input type="text" data-post_id="${'TODO'}"
+         <input type="text"
             class="outline-none p-4 w-full border border-solid border-gray-700 bg-input rounded-xl hover:border-white hover:border-opacity-70"
-            placeholder="Share your thoughts" id="commentInput" />
+            placeholder="Share your thoughts" id="subCommentInput" />
          <button class="-ml-20 px-5 py-2 rounded-xl bg-purple-900" type="submit">Post</button>
       `
 
@@ -101,7 +146,19 @@ document.querySelectorAll('.sub-comment').forEach(function (element) {
 
       form.addEventListener('submit', function (e) {
          e.preventDefault()
-         addSubNestedComent()
+         addNestedComment()
       })
    })
 })
+
+function addNestedComment(parent_id) {
+   const subCommentInput = document.getElementById('subCommentInput')
+   const subCommentInputValue = subCommentInput.value
+
+   const data = {
+      content: subCommentInputValue,
+      parent_id: parent_id
+   }
+
+   sendAjaxRequest('POST', '/comments', data, addCommentHandler)
+}
