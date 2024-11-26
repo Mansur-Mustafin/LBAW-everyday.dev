ec03b283d404aa6961752aaa27b5f54724118744
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-11-26 19:35:03 +0000

    fix: route comment, add comment policy

--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -16,4 +16,26 @@ class CommentPolicy
     {
         return $newsPost->comments->contains($comment) ? Response::allow() : Response::deny('Comment does not belong to the post!');
     }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Comment $comment): bool
+    {
+        return $user->id === $comment->author_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Comment $comment): bool
+    {
+        if($comment->upvotes != 0) return false;
+
+        if($comment->downvotes != 0) return false;
+
+        if ($comment->replies()->exists()) return false;
+        
+        return $user->id === $comment->author_id;
+    }
 }
