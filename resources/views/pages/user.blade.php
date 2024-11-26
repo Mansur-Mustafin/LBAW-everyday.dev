a5fced0655d62d8e21ee9ea116a2590fe2b50663
DGoiana <diogogoianam@gmail.com>
2024-11-26 21:52:31 +0000

    adding footer

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 
-<section class="flex flex-col tablet:flex-row laptop:border-x laptop:border-gray-700 self-center w-full laptop:m-auto laptop:max-w-[50.5rem] h-full">
+<section
+    class="flex flex-col tablet:flex-row laptop:border-x laptop:border-gray-700 self-center w-full laptop:m-auto laptop:max-w-[50.5rem] h-full">
     <main class="order-2 tablet:order-none flex-1">
         @php
             $isPostsActive = Route::currentRouteNamed('user.posts');
@@ -10,12 +11,16 @@
         @endphp
         <ul class="flex flex-wrap text-sm font-medium border-b border-gray-800 text-gray-400">
             <li class="me-2">
-                <a href="{{url('/users/'.$user->id.'/posts')}}" class="inline-block px-4 py-2 rounded-lg hover:bg-gray-800 hover:text-white {{$isPostsActive ? 'text-white' : ''}}">Posts</a>
-                <div class="{{$isPostsActive ? '' : 'hidden'}} border-b border-white border-1 w-4 rounded-xl mx-auto"></div>
+                <a href="{{url('/users/' . $user->id . '/posts')}}"
+                    class="inline-block px-4 py-2 rounded-lg hover:bg-gray-800 hover:text-white {{$isPostsActive ? 'text-white' : ''}}">Posts</a>
+                <div class="{{$isPostsActive ? '' : 'hidden'}} border-b border-white border-1 w-4 rounded-xl mx-auto">
+                </div>
             </li>
             <li class="me-2">
-                <a href="{{url('/users/'.$user->id.'/upvotes')}}" class="inline-block px-4 py-2 rounded-lg hover:bg-gray-800 hover:text-white {{$isUpvotesActive ? 'text-white' : ''}}">Upvotes</a>
-                <div class="{{$isUpvotesActive ? '' : 'hidden'}} border-b border-white border-1 w-4 rounded-xl mx-auto"></div>
+                <a href="{{url('/users/' . $user->id . '/upvotes')}}"
+                    class="inline-block px-4 py-2 rounded-lg hover:bg-gray-800 hover:text-white {{$isUpvotesActive ? 'text-white' : ''}}">Upvotes</a>
+                <div class="{{$isUpvotesActive ? '' : 'hidden'}} border-b border-white border-1 w-4 rounded-xl mx-auto">
+                </div>
             </li>
         </ul>
 
@@ -32,37 +37,41 @@
         @else
             <div class="container tablet:mx-auto w-full">
                 <h1 class="text-2xl font-semibold my-4 mx-4">{{ $title }}</h1>
-                <div data-last_page="{{ $news_posts->lastPage() }}" data-url="{{ $baseUrl }}" id="news-posts-container" class="grid grid-cols-1 gap-4">
+                <div data-last_page="{{ $news_posts->lastPage() }}" data-url="{{ $baseUrl }}" id="news-posts-container"
+                    class="grid grid-cols-1 gap-4">
                     @foreach ($news_posts as $news)
                         @include('partials.tile-post', ['news' => $news])
                     @endforeach
                 </div>
                 <div id="loading-icon" style="display: none;" class="my-6">
                     <img class="w-12 h-12 mx-auto" src="{{url('/assets/loading-icon.gif')}}" alt="Loading...">
-                </div>  
+                </div>
             </div>
         @endif
-    </main> 
+    </main>
 
     <aside class="order-1 tablet:order-none border-l border-gray-700">
         <header class="flex items-center p-4">
             <h2 class="font-bold text-lg flex-1">Profile</h2>
             @if (Auth::user()->id === $user->id)
-                <a href="{{url('/users/'.$user->id.'/edit')}}" class="text-input bg-white font-bold rounded-xl px-4 py-1 mx-2 self-end">Edit Profile</a>
+                <a href="{{url('/users/' . $user->id . '/edit')}}"
+                    class="text-input bg-white font-bold rounded-xl px-4 py-1 mx-2 self-end">Edit Profile</a>
             @elseif (Auth::user()->is_admin)
-                <a href="{{url('/admin/users/'.$user->id.'/edit')}}" class="text-input bg-white font-bold rounded-xl px-4 py-1 mx-2 self-end">Edit Profile</a>
+                <a href="{{url('/admin/users/' . $user->id . '/edit')}}"
+                    class="text-input bg-white font-bold rounded-xl px-4 py-1 mx-2 self-end">Edit Profile</a>
             @endif
             @can('follow', $user)
-                <button class="follow-button text-input bg-white font-bold rounded-xl px-4 py-1 mx-2 self-end" 
+                <button class="follow-button text-input bg-white font-bold rounded-xl px-4 py-1 mx-2 self-end"
                     data-user-id="{{ $user->id }}" data-action="follow">Follow</button>
             @endcan
             @can('unfollow', $user)
-                <button class="follow-button text-input bg-white font-bold rounded-xl px-4 py-1 mx-2 self-end" 
+                <button class="follow-button text-input bg-white font-bold rounded-xl px-4 py-1 mx-2 self-end"
                     data-user-id="{{ $user->id }}" data-action="unfollow">Unfollow</button>
             @endcan
         </header>
         <div class="rounded flex justify-center m-5">
-            <img src="{{$user->profile_image_path}}" alt="Your profile image" class="rounded-full w-48 h-48 object-cover border-2 border-white">
+            <img src="{{$user->profile_image_path}}" alt="Your profile image"
+                class="rounded-full w-48 h-48 object-cover border-2 border-white">
         </div>
         <div class="p-4">
             <p class="font-bold text-lg">
@@ -71,7 +80,7 @@
                     <span class="bg-red-400 text-gray-800 px-3 py-1 ml-1 rounded-full text-sm ">Admin</span>
                 @endif
             </p>
-            <span class="text-gray-300">{{$user->username}} </span> 
+            <span class="text-gray-300">{{$user->username}} </span>
             <span class="text-sm text-gray-500"> • Joined {{$user->created_at->format('F Y')}} </span>
             @if(Auth::check() and (Auth::id() == $user->id))
                 <p class="text-gray-300">{{$user->email}} </p>
@@ -80,7 +89,8 @@
         <div class="p-4 mt-4">
             <p>
                 <a href="{{route('users.following', $user->id)}}">
-                    <span class="mr-4">{{ $user->following()->count() }} <span class="text-gray-500">following</span></span>
+                    <span class="mr-4">{{ $user->following()->count() }} <span
+                            class="text-gray-500">following</span></span>
                 </a>
                 <a href="{{route('users.followers', $user->id)}}">
                     <span>{{ $user->followers()->count() }} <span class="text-gray-500">followers</span></span>
@@ -89,7 +99,7 @@
             <p>
                 <span class="mr-4">{{$user->rank}} <span class="text-gray-500">rank</span> </span>
                 @if(Auth::check() and (Auth::id() == $user->id))
-                    <span>{{$user->reputation}} <span class="text-gray-500">reputation</span> </span> 
+                    <span>{{$user->reputation}} <span class="text-gray-500">reputation</span> </span>
                 @endif
             </p>
         </div>
@@ -99,9 +109,10 @@
         </div> --}}
 
         <div class="p-4">
-        @if (Auth::check() and (Auth::id() == $user->id))
-            <a href="{{ url('/logout') }}" class="text-input bg-red-400 font-bold rounded-xl px-4 py-1 self-end">Logout</a>
-        @endif
+            @if (Auth::check() and (Auth::id() == $user->id))
+                <a href="{{ url('/logout') }}"
+                    class="text-input bg-red-400 font-bold rounded-xl px-4 py-1 self-end">Logout</a>
+            @endif
         </div>
 
         <!-- More info (only admin) -->
@@ -115,5 +126,11 @@
         @endif
     </aside>
 </section>
+<footer class="bg-background text-white py-4 w-full border-t border-solid border-gray-700 hidden" id="profile-footer">
+    <div class="flex justify-between items-center">
+        <p class="text-3xl">everyday.dev</p>
+        <p class="text-sm mr-5">&copy; {{ date('Y') }} everyday.dev | All rights reserved.</p>
+    </div>
+</footer>
 
 @endsection
