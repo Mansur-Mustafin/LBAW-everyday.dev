bd5c3f0cb6c8101f2e3a635d4d3de92a6adc29d7
Rubem Neto <rubemviscard2635@gmail.com>
2024-12-03 18:27:43 +0000

    refactor:move files

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -12,4 +12,40 @@ class TagController extends Controller
    {
     return Tag::all();
    } 
+
+   public function followTag(Request $request) 
+   {
+       try {
+           // TODO: Tag Policy
+           // $this->authorize('follow_tag', $user); 
+           Auth::user()->tags()->attach($request->tag);
+
+           return response()->json(['message' => "Successfully followed {$request->tag}"]);
+       } catch (AuthorizationException $e) {
+           return response()->json(['message' => "Cannot follow this {$request->tag}"], 403);
+       }
+   }
+
+   public function unfollowTag(Request $request) 
+   {
+       try {
+           // TODO: Tag Policy
+           // $this->authorize('follow_tag', $user);
+           Auth::user()->tags()->detach($request->tag);
+
+           return response()->json(['message' => "Successfully unfollowed {$request->tag}"]);
+       } catch (AuthorizationException $e) {
+           return response()->json(['message' => "Cannot unfollow this {$request->tag}"], 403);
+       } 
+   }
+
+   public function getFollowingTags(User $user) {
+       $tags = $user->tags()->paginate(10);
+
+       return response()->json([
+           'tags' => $tags,
+           'next_page' => $tags->currentPage() + 1,
+           'last_page' => $tags->lastPage()
+       ]);
+   }
 }
