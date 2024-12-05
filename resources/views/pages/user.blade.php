2a7a26191e7c84199c5103b44ad751ba11e7f325
Mourinha08 <afonsomoura485@gmail.com>
2024-12-05 14:31:50 +0000

    adding footer

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -134,12 +134,7 @@
         @endif
     </aside>
 </section>
-<footer class="bg-background text-white py-4 w-full border-t border-solid border-gray-700 hidden" id="profile-footer">
-    <div class="flex justify-between items-center">
-        <p class="text-3xl">everyday.dev</p>
-        <p class="text-sm mr-5">&copy; {{ date('Y') }} everyday.dev | All rights reserved.</p>
-    </div>
-</footer>
 
+@include('layouts.footer')
 
 @endsection
