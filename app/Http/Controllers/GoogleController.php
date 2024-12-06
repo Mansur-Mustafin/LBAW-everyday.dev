675b2911092d8d1eb2c7f5650934ab8c5bad5a5e
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-12-06 01:59:05 +0000

    feat: set password for OAuth users

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
-use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
 {
