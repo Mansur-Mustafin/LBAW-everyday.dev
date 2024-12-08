c78846492fbeb31d1d12dd4905900bcf68ccf4fd
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-12-08 04:26:52 +0000

    feat: separate recover password from login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,17 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-
     /**
      * Display a login form.
      */
@@ -58,60 +53,5 @@ class LoginController extends Controller
 
         return redirect()->route('news.recent')
             ->withSuccess('You have logged out successfully!');
-    }
-
-    public function showRecoverForm(Request $request)
-    {
-        return view('auth.recover');
-    }
-
-    public function showResetPasswordForm(Request $request)
-    {
-        $validated = $request->validate([
-            'email' => 'required|email',
-            'token' => 'required',
-        ]);
-
-        if (!$this->validateResetToken($validated['email'], $validated['token'])) {
-            return view('errors.invalid-link');
-        }
-
-        return view('auth.reset', ['email' => $validated['email'], 'token' => $validated['token']]);
-    }
-
-    public function recover(Request $request)
-    {
-        $validated = $request->validate([
-            'token' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|min:4|confirmed',
-        ]);
-
-        if (!$this->validateResetToken($validated['email'], $validated['token'])) {
-            return view('errors.invalid-link');
-        }
-
-        User::where('email', $validated['email'])
-            ->update(['password' => Hash::make($validated['password'])]);
-
-        DB::table('password_resets')->where('email', $validated['email'])->delete();
-
-        return redirect()->route('login')->withSuccess('Password reset successfully!');
-    }
-
-    private function validateResetToken(string $email, string $token)
-    {
-        $resetEntry = DB::table('password_resets')->where('email', $email)->first();
-
-        if (!$resetEntry || !Hash::check($token, $resetEntry->token)) {
-            return false;
-        }
-
-        $tokenCreatedTime = Carbon::parse($resetEntry->created_at);
-        if (Carbon::now()->diffInMinutes($tokenCreatedTime) > 5) {
-            return false;
-        }
-
-        return $resetEntry;
     }
 }
