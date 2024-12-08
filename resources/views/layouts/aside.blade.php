a78ef64228ff4240ce163796167fb4c3f1099106
Mourinha08 <afonsomoura485@gmail.com>
2024-12-08 17:32:45 +0000

    reordering aside

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,8 +1,8 @@
 <aside class="border-r border-solid border-gray-700 hidden laptop:h-full laptop:flex laptop:flex-col laptop:min-w-48 laptop:items-center">
-    <div class="sticky top-0 mt-2 laptop:flex laptop:flex-col laptop:items-center">
-        <a href="{{ route('news.my') }}" class="block p-2 rounded-md hover:bg-gray-700 mt-3">Your News</a>
-        <a href="{{ route('news.top') }}" class="block p-2 rounded-md hover:bg-gray-700">Top News</a>
+    <div class="sticky top-0 mt-3 laptop:flex laptop:flex-col laptop:items-center">
         <a href="{{ route('news.recent') }}" class="block p-2 rounded-md hover:bg-gray-700">Recent News</a>
+        <a href="{{ route('news.top') }}" class="block p-2 rounded-md hover:bg-gray-700">Top News</a>
+        <a href="{{ route('news.my') }}" class="block p-2 rounded-md hover:bg-gray-700">Your News</a>
     </div>
 
     <div class="fixed bottom-3 laptop:flex laptop:flex-col laptop:items-center">
@@ -13,10 +13,11 @@
 </aside>
 
 <aside class="min-w-16 border-r border-solid border-gray-700 laptop:hidden h-full flex flex-col items-center gap-2">
-    <div class="sticky top-0 mt-2 flex flex-col items-center gap-2">
-    <a href="{{ route('news.my') }}" class="block rounded-md p-2 hover:bg-gray-700 mt-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg></a>
-    <a href="{{ route('news.top') }}" class="block rounded-md p-2 hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg></a>
-    <a href="{{ route('news.recent') }}" class="block p-2 rounded-md hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round"><circle cx="12" cy="8" r="5"/><path d="M20 21a8 8 0 0 0-16 0"/></svg></a>
+    <div class="sticky top-0 mt-3 flex flex-col items-center gap-2">
+        <a href="{{ route('news.recent') }}" class="block p-2 rounded-md hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round"><circle cx="12" cy="8" r="5"/><path d="M20 21a8 8 0 0 0-16 0"/></svg></a>
+        <a href="{{ route('news.top') }}" class="block rounded-md p-2 hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg></a>
+        <a href="{{ route('news.my') }}" class="block rounded-md p-2 hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg></a>
+    </div>
     <div class="fixed bottom-3 flex flex-col items-center gap-2">
         <a href="{{ route('contacts') }}" class="block rounded-md p-2 hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16" fill="none" stroke="currentColor" class="bi bi-person-lines-fill"><path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z"/></svg></a>
         <a href="{{ route('about') }}" class="block rounded-md p-2 hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-info"><circle cx="12" cy="12" r="10" /><line x1="12" y1="16" x2="12" y2="12" /><line x1="12" y1="8" x2="12.01" y2="8" /></svg></a>
