7cee78a3cec3fb7c0c94d1b27dd71678376c7270
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-12-09 23:44:08 +0000

    add show all button

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -13,23 +13,40 @@ if (toggleFilterButton) {
   });
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.filter-section').forEach((section) => {
+    const header = section.querySelector('.filter-header');
+    const list = section.querySelector('.filter-list');
+    const chevronIcon = header.querySelector('svg');
+    const showAllButton = section.querySelector('.show-all-button');
+    
+    header.addEventListener('click', () => {
+      if (list.style.maxHeight) {
+        list.style.maxHeight = null;
+        chevronIcon.classList.remove('rotate-180');
+      } else {
+        list.style.maxHeight = list.scrollHeight + 'px';
+        chevronIcon.classList.add('rotate-180');
+      }
+    });
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".filter-section").forEach((section) => {
-      const header = section.querySelector(".filter-header");
-      const list = section.querySelector(".filter-list");
-      const chevronIcon = header.querySelector("svg");
-
-      header.addEventListener("click", () => {
-          if (list.style.maxHeight) {
-              list.style.maxHeight = null; 
-              chevronIcon.classList.remove("rotate-180");
-          } else {
-              list.style.maxHeight = list.scrollHeight + "px"; 
-              chevronIcon.classList.add("rotate-180");
-          }
-      });
+    showAllButton.addEventListener('click', () => {
+      if (showAllButton.dataset.value === 'show-all') {
+        document.querySelectorAll('li.hidden').forEach((li) => {
+          li.classList.remove('hidden');
+        });
+        list.style.maxHeight = list.scrollHeight + 'px';
+        showAllButton.innerHTML = 'Show Less';
+        showAllButton.dataset.value = 'show-less';
+      } else if (showAllButton.dataset.value === 'show-less') {
+        list.style.maxHeight = list.scrollHeight + 'px';
+        document.querySelectorAll('li.more-limit').forEach((li) => {
+          li.classList.add('hidden');
+        });
+        list.style.maxHeight = list.scrollHeight + 'px';
+        showAllButton.innerHTML = 'Show All';
+        showAllButton.dataset.value = 'show-all';
+      }
+    });
   });
 });
-
-
--- resources/views/partials/filter.blade.php
+++ resources/views/partials/filter.blade.php
@@ -5,25 +5,38 @@
         <button class="font-semibold text-sm hover:underline text-violet-400">Clear All</button>
     </div>
     
+    @php
+        $tags = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+        $limit = 6;
+    @endphp
 
     <div class="filter-section">
         <h4 class="filter-header mb-2 flex flex-row cursor-pointer">
             <span class="text-gray-400 text-sm grow">Tags</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-up"><path d="m18 15-6-6-6 6"/></svg>
+            <svg class="transition ease-out" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-up"><path d="m18 15-6-6-6 6"/></svg>
         </h4>
         
         <ul class="filter-list flex flex-col gap-1.5 overflow-hidden transition-[max-height] duration-300 ease-in-out max-h-0">
-            @foreach ([1,2,3,4,5,6,7,8,9,10,11,12] as $i)
-                <li class="flex items-center">
-                    <label class="text-sm font-medium text-gray-300 hover:text-white transition">
-                        <input type="checkbox" value="" class="form-checkbox w-4 h-4 text-violet-600 rounded ring-offset-gray-800 bg-gray-700 border-gray-600"/>
-                        &nbsp;Apple {{$i}}
-                    </label>
-                </li>
+            @foreach ($tags as $key => $tag)
+                @if ($key < $limit)
+                    <li class="flex items-center">
+                        <label class="text-sm font-medium text-gray-300 hover:text-white transition">
+                            <input type="checkbox" value="" class="form-checkbox w-4 h-4 text-violet-600 rounded ring-offset-gray-800 bg-gray-700 border-gray-600"/>
+                            &nbsp;Apple {{$tag}}
+                        </label>
+                    </li>
+                @else
+                    <li class="flex items-center hidden more-limit">
+                        <label class="text-sm font-medium text-gray-300 hover:text-white transition">
+                            <input type="checkbox" value="" class="form-checkbox w-4 h-4 text-violet-600 rounded ring-offset-gray-800 bg-gray-700 border-gray-600"/>
+                            &nbsp;Apple {{$tag}}
+                        </label>
+                    </li>
+                @endif
             @endforeach
 
             <li class="show-more-item">
-                <button class="show-more-button ml-2 text-sm text-violet-400 hover:underline">
+                <button class="show-all-button ml-2 text-sm text-violet-400 hover:underline" data-value="show-all">
                     Show All
                 </button>
             </li>
