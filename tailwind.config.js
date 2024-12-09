6a8f4bea90ec71158b3e7a667ba40b5707fd0cd5
DGoiana <diogogoianam@gmail.com>
2024-12-09 02:10:53 +0000

    adding tailwind typography and default configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,37 @@ export default {
       colors: {
         input: '#1b1f24',
         background: '#0f1217',
-      }
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            margin: '0',
+            padding: '0',
+            color: 'inherit',
+            background: 'none',
+            h1: {
+              margin: '0',
+              color: 'inherit',
+            },
+            h2: {
+              margin: '0',
+              color: 'inherit',
+            },
+            h3: {
+              margin: '0',
+              color: 'inherit',
+            },
+            p: {
+              margin: '0',
+              color: 'inherit',
+            },
+            span: {
+              backgroundColor: 'transparent',
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
