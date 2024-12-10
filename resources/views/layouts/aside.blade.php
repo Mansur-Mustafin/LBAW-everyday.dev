8c6fbc99b4421e5b94e01ff22213436331ca0c51
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-12-10 19:29:46 +0000

    fix: adapt user profle page

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -6,7 +6,7 @@
 
         <div class="border-2 rounded-xl border-gray-500 w-9/12 my-3"></div>
 
-        <a href="{{ route('news.bookmarks') }}" class="block p-2 rounded-md hover:bg-gray-700">Bookmarks</a>
+        <a href="{{ route('news.bookmark') }}" class="block p-2 rounded-md hover:bg-gray-700">Bookmarks</a>
     @endif
 </aside>
 
@@ -17,6 +17,6 @@
 
         <div class="border-2 rounded-xl border-gray-500 w-9/12 my-3"></div>
         
-        <a href="{{ route('news.bookmarks') }}" class="block rounded-md p-2 hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark-plus"> <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z" /> <line x1="12" x2="12" y1="7" y2="13" /> <line x1="15" x2="9" y1="10" y2="10" /> </svg></a>
+        <a href="{{ route('news.bookmark') }}" class="block rounded-md p-2 hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark-plus"> <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z" /> <line x1="12" x2="12" y1="7" y2="13" /> <line x1="15" x2="9" y1="10" y2="10" /> </svg></a>
     @endif
 </aside>
