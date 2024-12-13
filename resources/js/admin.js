0189d90030438cac58716b1527478e15bea6d9f9
Mourinha08 <afonsomoura485@gmail.com>
2024-12-13 19:10:03 +0000

    removing buttons when user is deleted on admin's dashboard

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -123,30 +123,52 @@ const buildUserCard = (user) => {
                 <h3 class="text-gray-400">${user.email}</h3>
               </div>
               ${
-                !user.is_admin 
-                ? user.status != 'blocked'
+                user.status !== 'deleted' 
+                  ? !user.is_admin 
+                    ? user.status != 'blocked'
+                      ? `
+                        <a id="${user.id}-block-button" data-url="${baseUrl}" class="block-button flex flex-col p-2 justify-center" href="">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ban">
+                            <circle cx="12" cy="12" r="10"/>
+                            <path d="m4.9 4.9 14.2 14.2"/>
+                          </svg>
+                        </a>
+                      `
+                      : `
+                        <a id="${user.id}-unblock-button" data-url="${baseUrl}" class="unblock-button flex flex-col p-2 justify-center" href="">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-dashed">
+                            <path d="M10.1 2.182a10 10 0 0 1 3.8 0"/>
+                            <path d="M13.9 21.818a10 10 0 0 1-3.8 0"/>
+                            <path d="M17.609 3.721a10 10 0 0 1 2.69 2.7"/>
+                            <path d="M2.182 13.9a10 10 0 0 1 0-3.8"/>
+                            <path d="M20.279 17.609a10 10 0 0 1-2.7 2.69"/>
+                            <path d="M21.818 10.1a10 10 0 0 1 0 3.8"/>
+                            <path d="M3.721 6.391a10 10 0 0 1 2.7-2.69"/>
+                            <path d="M6.391 20.279a10 10 0 0 1-2.69-2.7"/>
+                          </svg>
+                        </a >
+                      `
+                    : ''
+                  : ''
+              }              
+
+              ${
+                user.status !== 'deleted'
                   ? `
-                    <a id="${user.id}-block-button" data-url="${baseUrl}" class="block-button flex flex-col p-2 justify-center" href="">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ban"><circle cx="12" cy="12" r="10"/><path d="m4.9 4.9 14.2 14.2"/></svg>
+                    <a class="flex flex-col p-2 justify-center" href="${editProfileUrl}">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
+                    </a>
+
+                    <a id="${user.id}-delete-button" data-url="${baseUrl}" class="delete-button flex flex-col p-2 justify-center" href="">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash">
+                        <polyline points="3 6 5 6 21 6"/>
+                        <path d="M19 6l-1.34 14.22A2 2 0 0 1 15.67 22H8.33a2 2 0 0 1-1.99-1.78L5 6m5 0V4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2m-7 0h8"/>
+                      </svg>
                     </a>
                   `
-                  : `
-                    <a id="${user.id}-unblock-button" data-url="${baseUrl}" class="unblock-button flex flex-col p-2 justify-center" href="">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-dashed"><path d="M10.1 2.182a10 10 0 0 1 3.8 0"/><path d="M13.9 21.818a10 10 0 0 1-3.8 0"/><path d="M17.609 3.721a10 10 0 0 1 2.69 2.7"/><path d="M2.182 13.9a10 10 0 0 1 0-3.8"/><path d="M20.279 17.609a10 10 0 0 1-2.7 2.69"/><path d="M21.818 10.1a10 10 0 0 1 0 3.8"/><path d="M3.721 6.391a10 10 0 0 1 2.7-2.69"/><path d="M6.391 20.279a10 10 0 0 1-2.69-2.7"/></svg>
-                    </a >
-                  `
-                : ''
+                  : ''
               }
-              <a class="flex flex-col p-2 justify-center" href="${editProfileUrl}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
-              </a>
-
-              <a id="${user.id}-delete-button" data-url="${baseUrl}" class="delete-button flex flex-col p-2 justify-center" href="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash">
-                  <polyline points="3 6 5 6 21 6"/>
-                  <path d="M19 6l-1.34 14.22A2 2 0 0 1 15.67 22H8.33a2 2 0 0 1-1.99-1.78L5 6m5 0V4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2m-7 0h8"/>
-                </svg>
-              </a>
+              
           </div>
       </div>
       `;
