0507f7ffc6e192a69815cba55ecbf576ff21fa8a
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-12-15 16:46:45 +0000

    fix: hide comment buttons

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -138,7 +138,7 @@
                 @endif
 
                 <!-- Resto -->
-                <a class="p-2 rounded-xl cursor-pointer hover:text-rose-400 hover:bg-rose-700 hover:bg-opacity-50">
+                {{-- <a class="p-2 rounded-xl cursor-pointer hover:text-rose-400 hover:bg-rose-700 hover:bg-opacity-50">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                         class="lucide lucide-bookmark-plus">
@@ -154,7 +154,7 @@
                         <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
                         <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
                     </svg> </a>
-                </a>
+                </a> --}}
             </div>
         </div>
 
