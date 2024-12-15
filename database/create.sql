7c4482110a7a7b998f53bb038be229cdcd40e569
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-12-15 16:39:14 +0000

    fix: delete user

--- database/create.sql
+++ database/create.sql
@@ -727,6 +727,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
+-- Trigger 18 TODO: change in WIKI
 CREATE TRIGGER trigger_adjust_comment_votes
 AFTER INSERT OR DELETE OR UPDATE ON vote
 FOR EACH ROW
@@ -735,10 +737,16 @@ EXECUTE FUNCTION adjust_comment_votes();
 CREATE OR REPLACE FUNCTION anonymize_user_on_status_change()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.username := CONCAT('deleted_user_', OLD.id);
-    NEW.public_name := 'Anonymous';
-    NEW.email := CONCAT('deleted_', OLD.id, '@example.com');
-    NEW.password := '';
+    UPDATE "user"
+    SET 
+        username = CONCAT('deleted_user_', OLD.id),
+        public_name = 'Anonymous',
+        email = CONCAT('deleted_', OLD.id, '@example.com'),
+        password = NULL,
+        google_id = NULL,
+        status = 'deleted',
+        remember_token = NULL
+    WHERE id = OLD.id;
     
     DELETE FROM user_tag_subscribes WHERE user_id = OLD.id;
     DELETE FROM notification_settings WHERE user_id = OLD.id;
