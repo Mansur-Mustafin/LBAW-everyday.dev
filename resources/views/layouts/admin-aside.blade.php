cddbb4589c39535c96c3d7fee734f526fb18a83c
Mourinha08 <afonsomoura485@gmail.com>
2024-12-15 17:50:52 +0000

    adding button to responsiveness design

--- resources/views/layouts/admin-aside.blade.php
+++ resources/views/layouts/admin-aside.blade.php
@@ -55,5 +55,10 @@
                 <rect x="13" y="15" width="8" height="5" rx="1"/>
             </svg>
         </a>
+        <a href="{{ route('admin.reports') }}" class="flex p-2 rounded-md hover:bg-gray-700 gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-circle-warning"><path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z"/>
+                <path d="M12 8v4"/><path d="M12 16h.01"/>
+            </svg>
+        </a>
     </div>
 </aside>
