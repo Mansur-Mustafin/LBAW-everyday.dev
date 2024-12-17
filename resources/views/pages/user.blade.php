8c232d8dfe480d672e13b0eb88e6d793c7398924
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-12-17 17:50:47 +0000

    feat: add feedback on share button

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -63,7 +63,8 @@
                 <button class="follow-button text-input bg-white font-bold rounded-xl px-4 py-1 mx-2 self-end"
                     data-user-id="{{ $user->id }}" data-action="unfollow">Unfollow</button>
             @endcan
-            <button id="share-profile">
+            <button id="share-profile" class="relative">
+                <span class="copied-feedback absolute bottom-7 -left-4 opacity-0 transition-opacity duration-300 text-sm bg-input px-1.5 py-0.5 rounded-lg">copied</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                     class="lucide lucide-link">
