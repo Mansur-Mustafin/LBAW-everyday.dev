925ccc72a9eb2a2ff647f7ab0167a068cca94088
DGoiana <diogogoianam@gmail.com>
2024-12-17 11:18:11 +0000

    adding duplicate constraint verification in post

--- app/Http/Requests/NewsPost/StoreRequest.php
+++ app/Http/Requests/NewsPost/StoreRequest.php
@@ -9,8 +9,8 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|string|max:250',
-            'content' => 'required|string',
+            'title' => 'required|string|unique:news_post,title|max:250',
+            'content' => 'required|unique:news_post,content|string',
             'for_followers' => 'required|string',
             'image' => 'nullable|image|max:2048',
             'tags' => 'nullable|string',
