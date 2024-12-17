0acd7936bb4abf1a3adbef0f4dbbc4a0960326d4
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-12-17 03:54:50 +0000

    fix: change the loading buttn if form has errors

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -90,3 +90,27 @@ export function uploadBase64Image(dataUrl) {
 
   return blob;
 }
+
+export const tranformLoadingButton = (button) => {
+  console.log(button);
+
+  button.classList.remove('text-input', 'bg-white', 'font-bold', 'px-6', 'py-2');
+
+  button.classList.add(
+    'text-gray-100',
+    'font-semibold',
+    'pr-3',
+    'py-1',
+    'bg-[#4C4B63]',
+    'flex',
+    'items-center',
+    'justify-center'
+  );
+
+  button.disabled = true;
+
+  button.innerHTML = `
+      <img class="w-8 h-8" src="/assets/loading-icon.gif" alt="Loading...">
+      Sending...
+    `;
+};
