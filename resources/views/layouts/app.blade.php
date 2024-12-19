ff508de7a6b83ec42ff7f73368af5d6bd36a40b8
DGoiana <diogogoianam@gmail.com>
2024-12-19 18:53:27 +0000

    changes in the profile page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,7 @@
     @vite('resources/js/filter.js')
     @vite('resources/js/sort-by.js')
     @vite('resources/js/loading-button.js')
+    @vite('resources/js/profile.js')
 
 
     <script type="text/javascript">
