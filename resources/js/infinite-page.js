c511d6f55021f464ea7296f2e570f466dc79ae30
DGoiana <diogogoianam@gmail.com>
2024-12-21 03:42:40 +0000

    refactoring notifications

--- resources/js/infinite-page.js
+++ resources/js/infinite-page.js
@@ -25,16 +25,15 @@ const buildByRequest = async (url, buildUser, resultsDiv) => {
       loading = false;
       if (loadingIcon) loadingIcon.classList.add('hidden');
       lastPage = data.last_page;
-      if(data.data) {
+      if (data.data) {
         data.data.forEach((user) => {
           resultsDiv.innerHTML += buildUser(user);
         });
-      } 
+      }
     },
     method
   );
 };
-
 
 // Posts
 const postContainer = document.getElementById('news-posts-container');
@@ -83,7 +82,7 @@ if (postContainer) {
   };
 
   window.onload = refreshPage;
-  if(filter) {
+  if (filter) {
     initializeFilter();
     initializeSortByPopup();
   }
@@ -110,7 +109,7 @@ if (postContainer) {
         loading = false;
         loadingIcon?.classList.add('hidden');
         if (data.news_posts == '') {
-          console.log(data)
+          console.log(data);
           return;
         }
         postContainer.innerHTML += data.news_posts;
@@ -240,38 +239,38 @@ if (resultsDivNotification) {
     time_ago,
     is_viewed,
   }) => `
-    <div class="hover:bg-gray-900 p-4 flex">
-        <div class="w-8 h-8 mr-4 relative">
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                <g id="SVGRepo_iconCarrier">
-                    <path
-                        d="M19.3399 14.49L18.3399 12.83C18.1299 12.46 17.9399 11.76 17.9399 11.35V8.82C17.9399 6.47 16.5599 4.44 14.5699 3.49C14.0499 2.57 13.0899 2 11.9899 2C10.8999 2 9.91994 2.59 9.39994 3.52C7.44994 4.49 6.09994 6.5 6.09994 8.82V11.35C6.09994 11.76 5.90994 12.46 5.69994 12.82L4.68994 14.49C4.28994 15.16 4.19994 15.9 4.44994 16.58C4.68994 17.25 5.25994 17.77 5.99994 18.02C7.93994 18.68 9.97994 19 12.0199 19C14.0599 19 16.0999 18.68 18.0399 18.03C18.7399 17.8 19.2799 17.27 19.5399 16.58C19.7999 15.89 19.7299 15.13 19.3399 14.49Z"
-                        fill=${color}></path>
-                    <path
-                        d="M14.8297 20.01C14.4097 21.17 13.2997 22 11.9997 22C11.2097 22 10.4297 21.68 9.87969 21.11C9.55969 20.81 9.31969 20.41 9.17969 20C9.30969 20.02 9.43969 20.03 9.57969 20.05C9.80969 20.08 10.0497 20.11 10.2897 20.13C10.8597 20.18 11.4397 20.21 12.0197 20.21C12.5897 20.21 13.1597 20.18 13.7197 20.13C13.9297 20.11 14.1397 20.1 14.3397 20.07C14.4997 20.05 14.6597 20.03 14.8297 20.01Z"
-                        fill=${color}></path>
-                </g>
-            </svg>
-            ${
-              is_viewed
-                ? ''
-                : '<div class="related bg-red-400 rounded-full w-3 h-3 absolute bottom-5 left-5"></div>'
-            }
+    <div class="bg-input p-4 flex flex-col rounded-xl">
+      <div class="flex gap-2 items-center justify-between">
+        <div class="flex gap-2">
+          <img src="${imageSrc}"
+              alt="Profile Image" class="w-10 h-10 rounded-xl">
+          <div class="flex flex-col">
+            <span class="font-semibold text-gray-300">${triggeredBy}</span>
+            <span class="text-gray-500">${username}</span>
+          </div>
         </div>
-        <div class=" grow">
-            <div class="flex gap-2 items-center">
-                <img src="${imageSrc}"
-                    alt="Profile Image" class="w-10 h-10 rounded-xl">
-                <div class="flex flex-col">
-                  <span class="font-semibold text-gray-300">${triggeredBy}</span>
-                  <span class="text-gray-500">${username}</span>
-                </div>
-            </div>
-            <p class="mt-4">${text}</p>
-        </div>
+          <svg viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                  <path
+                      d="M19.3399 14.49L18.3399 12.83C18.1299 12.46 17.9399 11.76 17.9399 11.35V8.82C17.9399 6.47 16.5599 4.44 14.5699 3.49C14.0499 2.57 13.0899 2 11.9899 2C10.8999 2 9.91994 2.59 9.39994 3.52C7.44994 4.49 6.09994 6.5 6.09994 8.82V11.35C6.09994 11.76 5.90994 12.46 5.69994 12.82L4.68994 14.49C4.28994 15.16 4.19994 15.9 4.44994 16.58C4.68994 17.25 5.25994 17.77 5.99994 18.02C7.93994 18.68 9.97994 19 12.0199 19C14.0599 19 16.0999 18.68 18.0399 18.03C18.7399 17.8 19.2799 17.27 19.5399 16.58C19.7999 15.89 19.7299 15.13 19.3399 14.49Z"
+                      fill=${color}></path>
+                  <path
+                      d="M14.8297 20.01C14.4097 21.17 13.2997 22 11.9997 22C11.2097 22 10.4297 21.68 9.87969 21.11C9.55969 20.81 9.31969 20.41 9.17969 20C9.30969 20.02 9.43969 20.03 9.57969 20.05C9.80969 20.08 10.0497 20.11 10.2897 20.13C10.8597 20.18 11.4397 20.21 12.0197 20.21C12.5897 20.21 13.1597 20.18 13.7197 20.13C13.9297 20.11 14.1397 20.1 14.3397 20.07C14.4997 20.05 14.6597 20.03 14.8297 20.01Z"
+                      fill=${color}></path>
+              </g>
+          </svg>
+          ${
+            is_viewed
+              ? ''
+              : '<div class="related bg-red-400 rounded-full w-3 h-3 absolute bottom-5 left-5"></div>'
+          }
+      </div>
+      <div class="flex justify-between">
+        <p class="mt-4">${text}</p>
         <p class="text-gray-500 self-end">${time_ago}</p>
+      </div>
     </div>
   `;
 
