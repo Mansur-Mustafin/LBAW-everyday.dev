c382326cd7635266dc132ab306e249ee99967834
DGoiana <diogogoianam@gmail.com>
2024-12-21 17:32:22 +0000

    uniformizing admin pages and fixing tag page

--- resources/views/pages/admin/create-user.blade.php
+++ resources/views/pages/admin/create-user.blade.php
@@ -5,9 +5,9 @@
 @include('partials.success-popup')
 
 <section
-    class="flex flex-col laptop:border-x laptop:border-gray-700 self-center w-full laptop:m-auto laptop:max-w-[50.5rem] h-full">
-    <form method="POST" action="{{ url('/admin/register') }}" enctype="multipart/form-data"
-        class="px-3 flex flex-col gap-4 mt-4" id="admin-edit-profile">
+    class="flex flex-col laptop:border-x p-5 laptop:border-gray-700 self-center w-full laptop:m-auto laptop:max-w-[50.5rem] h-full">
+    <form method="POST" action="{{ url('/admin/register') }}" enctype="multipart/form-data" class="flex flex-col gap-4"
+        id="admin-edit-profile">
         @csrf
 
         <h3 class="font-bold text-lg flex-1">Profile Picture</h3>
@@ -18,7 +18,7 @@
                 <img class="rounded-full w-48 h-48 object-cover border-2 border-white" src=""
                     alt="Current Profile Image">
             </button>
-            <button class="bg-input rounded-3xl px-6 py-8 w-40 min-h-28" id="buttonAddImage"
+            <button class="bg-input rounded-3xl px-6 py-8 w-40 min-h-28 text-sm" id="buttonAddImage"
                 title="Click to upload new Image">
                 Upload image
                 <button type="button" id="deleteThumbnail" class="self-start hidden">
@@ -40,40 +40,43 @@
         <h3 class="font-bold text-lg flex-1">User Information</h3>
 
         <div class="flex flex-col">
-            <label class="font-bold text-sm mb-1">Public Name</label>
-            <input name="public_name" class="rounded-2xl bg-input outline-none p-3" placeholder="Public Name*" value="">
+            <label class="font-bold text-sm mb-2">Public Name</label>
+            <input name="public_name" class="rounded-2xl bg-input outline-none p-3 text-sm" placeholder="Public Name*"
+                value="">
             @error('public_name')
                 <span class="text-red-400 text-sm">{{ $message }}</span>
             @enderror
         </div>
 
         <div class="flex flex-col">
-            <label class="font-bold text-sm mb-1">Username</label>
-            <input name="username" class="rounded-2xl bg-input outline-none p-3" placeholder="Username*" value="">
+            <label class="font-bold text-sm mb-2">Username</label>
+            <input name="username" class="rounded-2xl bg-input outline-none p-3 text-sm" placeholder="Username*"
+                value="">
             @error('username')
                 <span class="text-red-400 text-sm">{{ $message }}</span>
             @enderror
         </div>
 
         <div class="flex flex-col">
-            <label class="font-bold text-sm mb-1">Email</label>
-            <input name="email" class="rounded-2xl bg-input outline-none p-3" placeholder="Email*" value="">
+            <label class="font-bold text-sm mb-2">Email</label>
+            <input name="email" class="rounded-2xl bg-input outline-none p-3 text-sm" placeholder="Email*" value="">
             @error('email')
                 <span class="text-red-400 text-sm">{{ $message }}</span>
             @enderror
         </div>
 
         <div class="flex flex-col">
-            <label class="font-bold text-sm mb-1">Reputation</label>
-            <input name="reputation" class="rounded-2xl bg-input outline-none p-3" placeholder="Reputation*" value="">
+            <label class="font-bold text-sm mb-2">Reputation</label>
+            <input name="reputation" class="rounded-2xl bg-input outline-none p-3 text-sm" placeholder="Reputation*"
+                value="">
             @error('reputation')
                 <span class="text-red-400 text-sm">{{ $message }}</span>
             @enderror
         </div>
 
         <div class="flex flex-col relative w-full">
-            <label class="font-bold text-sm mb-1">Password</label>
-            <input name="password" type="password" class="rounded-2xl bg-input outline-none p-3"
+            <label class="font-bold text-sm mb-2">Password</label>
+            <input name="password" type="password" class="rounded-2xl bg-input outline-none p-3 text-sm"
                 placeholder="New Password*">
             <span class="toggle-password material-icons cursor-pointer absolute inset-y-8 right-3 text-gray-500">
                 visibility_off
