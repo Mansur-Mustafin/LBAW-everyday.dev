7f84d6c65c0cda7d2912ddf3af21f5155bd3d5aa
DGoiana <diogogoianam@gmail.com>
2024-12-21 16:56:53 +0000

    fixing js errors

--- resources/js/profile.js
+++ resources/js/profile.js
@@ -1,10 +1,12 @@
 const profileOptions = document.getElementById('profile-options');
 const profileOptionsPopup = document.getElementById('profile-options-popup');
 
-profileOptions.addEventListener('click', (event) => {
-  if (profileOptionsPopup.classList.contains('opacity-0')) {
-    profileOptionsPopup.classList.replace('opacity-0', 'opacity-100');
-  } else {
-    profileOptionsPopup.classList.replace('opacity-100', 'opacity-0');
-  }
-});
+if (profileOptions) {
+  profileOptions.addEventListener('click', (event) => {
+    if (profileOptionsPopup.classList.contains('opacity-0')) {
+      profileOptionsPopup.classList.replace('opacity-0', 'opacity-100');
+    } else {
+      profileOptionsPopup.classList.replace('opacity-100', 'opacity-0');
+    }
+  });
+}
