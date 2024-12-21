c49b9b8e76a2420f0e2a2d6a6c7aa2c6a924fa72
DGoiana <diogogoianam@gmail.com>
2024-12-21 18:23:21 +0000

    slight changes in edit user pages

--- resources/views/pages/edit-user.blade.php
+++ resources/views/pages/edit-user.blade.php
@@ -3,12 +3,10 @@
 @section('content')
 
 <section
-    class="flex flex-col laptop:border-x laptop:border-gray-700 self-center w-full laptop:m-auto laptop:max-w-[50.5rem] h-full">
-
-    <h2 class="text-2xl font-semibold my-4 mx-4">Edit Profile</h2>
+    class="flex flex-col p-5 laptop:border-x laptop:border-gray-700 self-center w-full laptop:m-auto laptop:max-w-[50.5rem] h-full">
 
     <form method="POST" action="{{ route('user.update', $user->id) }}" enctype="multipart/form-data"
-        class="px-3 flex flex-col gap-4 mt-4">
+        class="flex flex-col gap-4">
         @csrf
         @method('PUT')
         <h3 class="font-bold text-lg flex-1">Profile Picture</h3>
@@ -19,9 +17,9 @@
                 <img class="rounded-full w-48 h-48 object-cover border-2 border-white"
                     src="{{$user->profileImage->url}}" alt="Current Profile Image">
             </button>
-            <button class="hidden bg-input rounded-3xl px-6 py-8 w-40 min-h-28" id="buttonAddImage"
+            <button class="hidden bg-input rounded-3xl px-6 py-8 w-40 min-h-28 text-sm" id="buttonAddImage"
                 title="Click to upload new Image">
-                Click to Upload Image
+                Upload image
             </button>
             <button type="button" id="deleteThumbnail" class="self-start">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -43,7 +41,7 @@
 
         <div class="flex flex-col">
             <label class="font-bold text-sm mb-2">Public Name</label>
-            <input name="public_name" class="rounded-2xl bg-input outline-none p-3" placeholder="Public Name*"
+            <input name="public_name" class="rounded-2xl bg-input outline-none p-3 text-sm" placeholder="Public Name*"
                 value="{{ old('public_name', $user->public_name) }}">
             @error('public_name')
                 <span class="text-red-400 text-sm">{{ $message }}</span>
@@ -52,34 +50,34 @@
 
         <div class="flex flex-col">
             <label class="font-bold text-sm mb-2">Username</label>
-            <input name="username" class="rounded-2xl bg-input outline-none p-3" placeholder="Username*"
+            <input name="username" class="rounded-2xl bg-input outline-none p-3 text-sm" placeholder="Username*"
                 value="{{ old('username', $user->username) }}">
             @error('username')
                 <span class="text-red-400 text-sm">{{ $message }}</span>
             @enderror
         </div>
-        
+
         <div class="flex flex-col">
             <label class="font-bold text-sm mb-2">Email</label>
-            <input name="email" class="disabled rounded-2xl bg-input outline-none p-3" placeholder="Email*"
+            <input name="email" class="disabled rounded-2xl bg-input outline-none p-3 text-sm" placeholder="Email*"
                 value="{{ old('email', $user->email) }}">
             @error('email')
                 <span class="text-red-400 text-sm">{{ $message }}</span>
             @enderror
         </div>
 
-        <h3 class="font-bold text-lg flex-1">Reset Password</h3>
-
-        
-            
-        <p class="text-sm text-gray-500">Leave the password fields empty if you do not wish to reset your password.</p>
+        <div>
+            <h3 class="font-bold text-lg flex-1">Reset Password</h3>
+            <p class="text-sm text-gray-500">Leave the password fields empty if you do not wish to reset your password.
+            </p>
+        </div>
 
         @if (!is_null($user->password))
             <div class="flex flex-col relative w-full">
                 <label class="font-bold text-sm mb-2">Old Password</label>
-                <input name="old_password" type="password" class="rounded-2xl bg-input outline-none p-3"
+                <input name="old_password" type="password" class="rounded-2xl bg-input outline-none p-3 text-sm"
                     placeholder="Old Password*">
-                <span class="toggle-password material-icons cursor-pointer absolute inset-y-8 right-3 text-gray-500">
+                <span class="toggle-password material-icons cursor-pointer absolute inset-y-9 right-3 text-gray-500">
                     visibility_off
                 </span>
                 @error('old_password')
@@ -90,9 +88,9 @@
 
         <div class="flex flex-col relative w-full">
             <label class="font-bold text-sm mb-2">New Password</label>
-            <input name="new_password" type="password" class="rounded-2xl bg-input outline-none p-3"
+            <input name="new_password" type="password" class="rounded-2xl bg-input outline-none p-3 text-sm"
                 placeholder="New Password*">
-            <span class="toggle-password material-icons cursor-pointer absolute inset-y-8 right-3 text-gray-500">
+            <span class="toggle-password material-icons cursor-pointer absolute inset-y-9 right-3 text-gray-500">
                 visibility_off
             </span>
             @error('new_password')
@@ -102,20 +100,21 @@
 
         <div class="flex flex-col relative w-full">
             <label class="font-bold text-sm mb-2">Confirm Password</label>
-            <input name="new_password_confirmation" type="password" class="rounded-2xl bg-input outline-none p-3"
-                placeholder="Confirm Password*">
-            <span class="toggle-password material-icons cursor-pointer absolute inset-y-8 right-3 text-gray-500">
+            <input name="new_password_confirmation" type="password"
+                class="rounded-2xl bg-input outline-none p-3 text-sm" placeholder="Confirm Password*">
+            <span class="toggle-password material-icons cursor-pointer absolute inset-y-9 right-3 text-gray-500">
                 visibility_off
             </span>
             @error('new_password_confirmation')
                 <span class="text-red-400 text-sm">{{ $message }}</span>
             @enderror
-        </div>            
+        </div>
 
         <div class="flex gap-2 self-end mb-4">
             <a href="{{ url('/users/' . $user->id . '/posts') }}"
                 class="text-input bg-red-400 font-bold rounded-xl px-6 py-2">Cancel</a>
-            <button class="loading-button text-input bg-white font-bold rounded-xl px-6 py-2" type="submit">Save Changes</button>
+            <button class="loading-button text-input bg-white font-bold rounded-xl px-6 py-2"
+                type="submit">Save</button>
         </div>
 
     </form>
