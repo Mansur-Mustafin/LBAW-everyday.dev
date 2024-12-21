f843453eb5cd68c6e5ad0078e5892ffd9c026efa
Mourinha08 <afonsomoura485@gmail.com>
2024-12-21 20:44:05 +0000

    centering placeholder search on admin pages

--- resources/views/pages/admin/admin.blade.php
+++ resources/views/pages/admin/admin.blade.php
@@ -7,7 +7,7 @@
 <div
   class="flex flex-col laptop:border-x laptop:border-gray-700 self-center w-full laptop:m-auto laptop:max-w-[50.5rem] h-full p-5">
   <div class="flex gap-2 items-center mt-20 justify-between">
-    <div class="flex flex-grow text-white bg-input rounded-2xl p-3">
+    <div class="flex flex-grow text-white bg-input rounded-2xl p-3 items-center">
       <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="28" fill="white" viewBox="0 0 30 30" class="mr-1">
         <path
           d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z">
