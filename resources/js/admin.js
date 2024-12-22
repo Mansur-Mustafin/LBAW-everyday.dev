05a8ce7e0c84dccfdf107376f7abe28427950df7
Rubem Neto <rubemviscard2635@gmail.com>
2024-12-22 18:25:02 +0000

    fix:hide block on pending users

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -102,7 +102,7 @@ const buildUserCard = (user) => {
                   <a title="block" id="${
                     user.id
                   }-block-button" data-url="${baseUrl}" class="block-button flex flex-col p-2 justify-center ${
-                  user.status != 'blocked' ? '' : 'hidden'
+                  user.status != 'blocked' && user.status != 'pending' ? '' : 'hidden'
                 }" href="">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ban">
                       <circle cx="12" cy="12" r="10"/>
