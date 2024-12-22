94f88d8551399ea906d24acd008ab26b520ecd52
Mansur Mustafin <mustafin.mansur02@gmail.com>
2024-12-22 00:04:06 +0000

    fix: check unique title case update

--- app/Http/Requests/NewsPost/UpdateRequest.php
+++ app/Http/Requests/NewsPost/UpdateRequest.php
@@ -6,33 +6,26 @@ use App\Models\NewsPost;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Validator;
+use Illuminate\Validation\Rule;
 
 class UpdateRequest extends FormRequest
 {
+    // TODO: add here ignore for same post. with Rule.
     public function rules(): array
     {
         return [
-            'title' => 'required|string|max:250',
+            'title' => [
+                'required',
+                'string',
+                'max:250',
+                Rule::unique('news_post')->ignore($this->route('news_post')->id),
+            ],
             'content' => 'required|string',
             'for_followers' => 'nullable|string',
             'image' => 'nullable|image|max:2048',
             'tags' => 'nullable|string',
             'remove_image' => 'required|string',
             'content_images' => 'nullable|string'
-        ];
-    }
-
-    public function after(): array
-    {
-        return [
-            function (Validator $validator) {
-                if (NewsPost::where('author_id', Auth::user()->id)
-                    ->where('title', $this->input('title'))
-                    ->where('content', $this->input('content'))
-                    ->exists()) {
-                    $validator->errors()->add('title', 'A post with the same title and content already exists.');
-                }
-            }
         ];
     }
 }
